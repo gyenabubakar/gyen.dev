ac6db3a03d6488fd9af6c0ec43e58a3dfe81eb5c
Gyen Abubakar <indehyde@gmail.com>
2021-12-11 08:27:11 +0000

    added tests and buttons

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
@@ -12,14 +13,43 @@ const Home: NextPage = () => {
 
 			<header>
 				<div className="container">
-					<div className="flex-end">
-						<a href="https://blog.gyen.dev" className="checkout-btn font-md">
+					<div className="flex-between">
+						<a
+							href="https://github.com/gyenabubakar/portfolio"
+							className="btn github font-md"
+						>
+							<img src="/github.svg" alt="github" />
+						</a>
+
+						<a
+							href="https://blog.gyen.dev"
+							className="btn checkout-btn font-md"
+						>
 							<span className="color-2">~$</span>&nbsp;
 							<span className="color-1">git checkout blog</span>
 						</a>
 					</div>
 				</div>
 			</header>
+
+			<main>
+				<div className="container">
+					<div className="row">
+						<div className="text-content">
+							<h1 className="font-bold">
+								Portfolio&apos;s
+								<br />
+								being developed.
+							</h1>
+							<h3 className="font-bold color-1">Will be deployed very soon.</h3>
+						</div>
+
+						<div className="img-illustration">
+							<img src="/imac.png" alt="iMac illustration" />
+						</div>
+					</div>
+				</div>
+			</main>
 		</div>
 	);
 };
