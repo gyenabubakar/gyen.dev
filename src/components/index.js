fdfff2f6f49bb27ef42f18f4ab0322652ae4ddfc
Gyen Abubakar <gyen@gyensmac.local>
2025-10-05 14:10:08 +0000

    feat: build minimalist portfolio with dark mode, recommendations, and experience sections

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as PageHeader } from './header.astro';
+export { default as Technologies } from './technologies.astro';
+export { default as Experience } from './experience.astro';
+export { default as Recommendations } from './recommendations.astro';
+export { default as Footer } from './footer.astro';
